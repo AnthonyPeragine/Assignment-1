00a62bb4899381f5a087708ac7ad4d4fe6badd32
peraa0837 <peraa0837@HRH-IC0027324.wrdsb.ca>
2017-02-10 09:49:40 -0500

    Final Commit for A1Q5

--- src/A1Q5.java
+++ src/A1Q5.java
@@ -2,6 +2,8 @@
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
 
 /*
  * To change this template, choose Tools | Templates
@@ -25,7 +27,49 @@ public class A1Q5 {
         
         //Creating karel the robot
         Robot karel = new Robot(kitchener, 3, 3, Direction.EAST);
+        karel.setLabel("K");
         //Creating bob the robot
         Robot maria = new Robot(kitchener, 0, 1, Direction.WEST);
+        maria.setLabel("M");
+        //Creates the things
+        new Thing(kitchener, 0, 0);
+        new Thing(kitchener, 1, 0);
+        new Thing(kitchener, 1, 1);
+        new Thing(kitchener, 1, 2);
+        new Thing(kitchener, 2, 2);
+        
+        //Creates walls in kitchener
+        new Wall(kitchener, 2, 3, Direction.NORTH);
+        new Wall(kitchener, 2, 3, Direction.EAST);
+        new Wall(kitchener, 2, 3, Direction.WEST);
+        new Wall(kitchener, 3, 3, Direction.EAST);
+        new Wall(kitchener, 3, 3, Direction.SOUTH);
+        
+        //Executes the routine for maria and karel
+        
+        maria.move();
+        maria.turnLeft();
+        maria.pickThing();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        maria.move();
+        maria.pickThing();
+        karel.move();
+        karel.pickThing();
+        karel.move();
+        karel.turnLeft();
+        karel.pickThing();
+        maria.turnLeft();
+        maria.move();
+        maria.pickThing();
+        
+        
+        
+        
+        
     }
 }
